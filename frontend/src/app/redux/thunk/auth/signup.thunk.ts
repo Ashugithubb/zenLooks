c31eaa5cd206ff02f4e33bfc84da7f8fb1019ce7
Ashutosh Kumar <itsray650@gmail.com>
2025-10-29 10:43:23 +0530

    /user api

--- frontend/src/app/redux/thunk/auth/signup.thunk.ts
+++ frontend/src/app/redux/thunk/auth/signup.thunk.ts
@@ -10,7 +10,7 @@ export const signupUser = createAsyncThunk(
   async (data: SignupFormData, thunkAPI) => {
     try {
       const response = await axios.post(
-        'http://localhost:3001/user',
+        `${process.env.NEXT_PUBLIC_API_URL}/user`,
         data,
         { withCredentials: true }
       );
