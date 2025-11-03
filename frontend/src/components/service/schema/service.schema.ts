a422ef444943053a23bccd507bac2146f45ce750
Ashutosh <itsray650@gmail.com>
2025-11-03 16:13:00 +0530

    new ui

--- frontend/src/components/service/schema/service.schema.ts
+++ frontend/src/components/service/schema/service.schema.ts
@@ -8,15 +8,15 @@ export enum Gender {
 export const serviceSchema = z.object({
   title: z.string()
     .trim()
-    .min(3, { message: 'Please enter Service Name' }),
+    .min(3, { message: 'Please enter service name' }),
 
   description: z.string()
     .trim()
     .min(6, { message: 'Please enter description' }),
   price: z.number().min(1, { message: 'Please enter regular price' }),
   time: z.number().min(1, { message: "Please enter duration" }),
-  discount: z.number().max(100,{message:"Discount cannot be mor then 100%"}).optional(),
-  category: z.enum(Gender,{message:"Please Select Category"}),
+  discount: z.number().max(100,{message:"Discount cannot be more then 100%"}).optional(),
+  category: z.enum(Gender,{message:"Please select category"}),
   imageUrl: z.string().optional()
 });
 
