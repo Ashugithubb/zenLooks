1e3a514fd31ad149081e0850512e229728f3b3c2
Ashutosh Kumar <itsray650@gmail.com>
2025-11-06 12:51:25 +0530

    final

--- frontend/src/app/redux/hook/useAuthCleanup.ts.ts
+++ frontend/src/app/redux/hook/useAuthCleanup.ts.ts
@@ -1,26 +1,11 @@
-"use client";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
-import { useAppDispatch } from "@/app/redux/hook/hook";
-import { clearUser } from "../slice/login.slice";
-
 
 export default function useAuthCleanup() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    // Wait 5 seconds before checking token
-    const timeout = setTimeout(() => {
-      const token = Cookies.get("access_token");
-
-      if (!token) {
-        console.log("Token missing → Clearing localStorage and Redux (after 5s)");
-        localStorage.clear();
-        dispatch(clearUser());
-      }
-    }, 5000); // 5 seconds delay
-
-    // cleanup
-    return () => clearTimeout(timeout);
-  }, [dispatch]);
+  const token = Cookies.get("access_token");
+  console.log("token",token);
+  if (!token) {
+    console.log("Token missing → Clearing localStorage");
+    localStorage.clear();
+  }
 }
